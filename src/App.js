6b5f8a81722f3a68467e92940bdfcaa7f1c9c786
Esteban Cardona Alvarez <esteban.alvarez@co-it00999.globant.com>
2018-09-03 16:44:15 -0500

    Creating initial structure for ReactTutorial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react'
+import FormContainer from './js/components/container/FormContainer'
+
+//  import './scss/main.scss'
+
+export default class App extends Component {
+  render () {
+    return (
+      <Fragment>
+        <FormContainer />
+      </Fragment>
+    )
+  }
+}
